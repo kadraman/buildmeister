59a9ba339315ec88eb4b7a69c9b293c2ff95736f
kevinalee <kevinalee@me.com>
2008-12-30 20:51:23 +0000

    Added corrected mysql version

--- db/createSchemaVersionTable.mysql.sql
+++ db/createSchemaVersionTable.mysql.sql
@@ -1,10 +1,11 @@
 CREATE TABLE changelog (
-  change_number INTEGER NOT NULL,
+  change_number BIGINT NOT NULL,
   delta_set VARCHAR(10) NOT NULL,
-  start_dt DATETIME NOT NULL,
-  complete_dt DATETIME NULL,
+  start_dt TIMESTAMP NOT NULL,
+  complete_dt TIMESTAMP NULL,
   applied_by VARCHAR(100) NOT NULL,
   description VARCHAR(500) NOT NULL
-)
+);
 
 ALTER TABLE changelog ADD CONSTRAINT Pkchangelog PRIMARY KEY (change_number, delta_set)
+;
